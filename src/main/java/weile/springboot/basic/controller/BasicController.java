fa2a67bee6c558235895d3dbe11f9087e3f66019
weiyue <bo.xu@163.com>
2018-07-16 18:26:31 +0800

    add redis test

---
+++ src/main/java/weile/springboot/basic/controller/BasicController.java
@@ -0,0 +1,36 @@
+package weile.springboot.basic.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import weile.Config;
+import weile.springboot.basic.RedisService;
+
+@RestController
+@EnableAutoConfiguration
+public class BasicController {
+
+
+    @Autowired
+    private Config config;
+
+
+    @Autowired
+    private RedisService redisService;
+
+    @RequestMapping(value = "/insert")
+    public boolean insert() {
+        return redisService.set("name", "cc");
+    }
+
+    @RequestMapping(value = "/get")
+    public String get() {
+        return redisService.get("name").toString();
+    }
+
+    @RequestMapping("/hello")
+    String hello() {
+        return "Hello World!" + config.getTestInfo() + config.getTestInfo1();
+    }
+}
