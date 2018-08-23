3d4d981047727ec7aa4ea2327b36d9d7cb2306ee
weiyue <bo.xu@163.com>
2018-08-23 14:41:29 +0800

    temp push

--- src/main/java/weile/Booter.java
+++ src/main/java/weile/Booter.java
@@ -1,11 +1,18 @@
 package weile;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Booter {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     public static void main(String[] args) {
         SpringApplication.run(Booter.class,args);
